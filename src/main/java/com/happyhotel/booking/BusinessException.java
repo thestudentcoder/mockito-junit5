67a297f20a291a0a7f1c90cf7d2ec8e5d42468bd
Wayne Chan <wchan.it@outlook.com>
2021-06-11 18:09:27 -0500

    initial commit

---
+++ src/main/java/com/happyhotel/booking/BusinessException.java
@@ -0,0 +1,5 @@
+package com.happyhotel.booking;
+
+public class BusinessException extends RuntimeException {
+
+}
---
+++ src/test/java/com/happyhotel/booking/Test00SanityCheck.java
@@ -0,0 +1,12 @@
+package com.happyhotel.booking;
+
+import org.junit.jupiter.api.Test;
+
+class Test00SanityCheck {
+
+	@Test
+	void test() {
+		// should pass
+	}
+
+}
